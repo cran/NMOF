303d612e2f621ccacfdf80caf49d865af77162e5
Enrico Schumann <es@enricoschumann.net>
2013-06-08 00:00:00 +0000

    version 0.28-0

--- R/internals.R
+++ R/internals.R
@@ -41,9 +41,12 @@ mRN <- function(m, n)
 ##
 
 mcList <- function(mc.control) {
-    mc.settings <- list(mc.preschedule = TRUE, mc.set.seed = TRUE,
-                        mc.silent = FALSE, mc.cores = getOption("cores"),
-                        mc.cleanup = TRUE)
+    mc.settings <- list(mc.preschedule = TRUE,
+                        mc.set.seed = TRUE,
+                        mc.silent = FALSE,
+                        mc.cores = getOption("mc.cores", 2L),
+                        mc.cleanup = TRUE,
+                        mc.allow.recursive = TRUE)
     checkList(mc.control, mc.settings, "'mc.control'")
     mc.settings[names(mc.control)] <- mc.control
     mc.settings
@@ -76,3 +79,18 @@ Chapters <- c("Introduction",
               "Portfolio optimization",
               "Econometric models",
               "Calibrating option pricing models")
+
+## dividends until expiry (used in vanillaOption*)
+
+due <- function(D, tauD, tau, q) {
+    if (any(q != 0) && any(D != 0))
+        stop("dividend rate and dividend amount supplied")
+    if (any(D != 0) && length(D) != length(tauD))
+        stop("number of dividends and number times-to-dividend differ")
+    if (any(D != 0)) {
+        D2keep <-  tauD <= tau & tauD > 0
+        D <- D[D2keep]
+        tauD <- tauD[D2keep]
+    }
+    list(tauD = tauD, D = D)
+}
